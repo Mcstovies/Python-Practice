2477794a1d8ab5224f5b1b3045959267f546bc1c
Adam Clarke <adamclarke0905@gmail.com>
2025-02-17 22:48:38 +0000

    Avoiding syntax erros with strings pg 62

--- index.py
+++ index.py
@@ -1,14 +1,29 @@
+# Using variables in strings
+
 first_name = "adam"
 last_name = "clarke"
-full_name = f"{first_name} {last_name}"
-message = f"Hello, {full_name.title()}!"
+full_name = f"{first_name} {last_name}" # f-strings(format) 
+message = f"Hello, {full_name.title()}!" # Title changes case in a string
 print(message)
 
 # Whitespaces are commonly used to clean up user input before its stored in a program
+
 favourite_language = 'python '
-favourite_language = favourite_language.rstrip()
+favourite_language = favourite_language.rstrip() # Removes extra space from the right side
 print(favourite_language)
 
 favourite_language = ' python'
-favourite_language = favourite_language.strip()
+favourite_language = favourite_language.lstrip() # Removes extra space from the left side
 print(favourite_language)
+
+favourite_language = ' python'
+favourite_language = favourite_language.strip() # Removes extra space from both sides
+print(favourite_language)
+
+# Removing Prefixes
+
+nostarch_url = 'https://nostarch.com'
+simple_url = nostarch_url.removeprefix('https://')
+print(simple_url)
+
+# Avoiding Syntax Erros with Strings
