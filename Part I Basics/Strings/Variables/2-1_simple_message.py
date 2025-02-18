283b24dd7719d74d513d4d8fd86ddfbb7efb234e
Adam Clarke <adamclarke0905@gmail.com>
2025-02-18 00:59:42 +0000

    variables exercises completed

---
+++ Part I Basics/Strings/Variables/2-1_simple_message.py
@@ -0,0 +1,5 @@
+# 2-1. Simple Message: Assign a message to a variable, and then print that
+# message.
+
+message = "Good luck on learning Python, Adam!"
+print(message)
---
+++ Part I Basics/Strings/Variables/2-2_simple_messages.py
@@ -0,0 +1,8 @@
+# 2-2. Simple Messages: Assign a message to a variable, and print that message.
+# Then change the value of the variable to a new message, and print the new
+# message.
+
+message = "Good luck on learning Python, Adam!"
+print(message)
+message = "Do it for her!"
+print(message)
