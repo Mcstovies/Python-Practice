ebcb7a5d9c6601f78d7a3c60b995f75610e56af0
Adam Clarke <adamclarke0905@gmail.com>
2025-02-19 00:59:44 +0000

    completed numbers and comment section of book

---
+++ Part I Basics/Numbers/2-10_favourite_number.py
@@ -0,0 +1,7 @@
+# 2-10. Favorite Number: Use a variable to represent your favorite number. Then,
+# using that variable, create a message that reveals your favorite number. Print
+# that message.
+
+favnum = 7
+message = f"This is my favourite number: {favnum}" # I need to remember to put the f at the beginning
+print(message)
