1e534e0b7b5ac1869845ed4951a59e7bc13bf140
Adam Clarke <adamclarke0905@gmail.com>
2025-02-22 00:42:37 +0000

    first lsit exercises completed

---
+++ Part I Basics/Chapter_3/What_Is_A_List/3-2_greetings.py
@@ -0,0 +1,14 @@
+# 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just
+# printing each person’s name, print a message to them. The text of each mes-
+# sage should be the same, but each message should be personalized with the
+# person’s name
+
+people = ['Adam', 'Lauren', 'Charlie', 'Lewis']
+message = f'Hey {people[0]}, how are you?'
+print(message)
+message = f'Hey {people[1]}, I love you!'
+print(message)
+message = f'Hey {people[2]}, Want to play Runescape?'
+print(message)
+message = f'Hey {people[3]}, YOU ARE THE BEST BOY!'
+print(message)
---
+++ Part I Basics/Chapter_3/What_Is_A_List/3-3_your_own_list.py
@@ -0,0 +1,12 @@
+# 3-3. Your Own List: Think of your favorite mode of transportation, such as a
+# motorcycle or a car, and make a list that stores several examples. Use your list
+# to print a series of statements about these items, such as “I would like to own a
+# Honda motorcycle.”
+
+cars = ['Aston Martin', 'Jaguar', 'BMW']
+message = f'One day I would like to own an {cars[0]}.'
+print(message)
+message = f'{cars[1]} are really expensive but apparently have gotten worse over the years.'
+print(message)
+message = f'Only total wanks drive {cars[2]}'
+print(message)
