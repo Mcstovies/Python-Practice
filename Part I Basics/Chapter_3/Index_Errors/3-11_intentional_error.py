b5f10512c48b43d018693dfb36b722be3e060083
Adam Clarke <adamclarke0905@gmail.com>
2025-03-12 16:31:51 +0100

    finished intro to lists

---
+++ Part I Basics/Chapter_3/Index_Errors/3-11_intentional_error.py
@@ -0,0 +1,7 @@
+# 3-11. Intentional Error: If you haven’t received an index error in one of your pro
+# grams yet, try to make one happen. Change an index in one of your programs 
+# to produce an index error. Make sure you correct the error before closing the 
+# program.
+
+uk = ['Scotland', 'England', 'Wales', 'Northern Ireland']
+print(uk[4]) # Gives the error list index out of range because Python counts from 0 and this goes beyond whats actually in the list
